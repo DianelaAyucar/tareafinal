31339bed7662e26585373ab05d69e1915903596a
Diane <139197312+DianelaAyucar@users.noreply.github.com>
2023-10-09 23:21:52 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+# Germán Andrés Xander 2023
+
+from machine import Pin, Timer, unique_id
+import dht
+import time
+import json
+import ubinascii
+from collections import OrderedDict
+from settings import SERVIDOR_MQTT
+from umqtt.robust import MQTTClient
+
+CLIENT_ID = ubinascii.hexlify(unique_id()).decode('utf-8')
+
+mqtt = MQTTClient(CLIENT_ID, SERVIDOR_MQTT,
+                  port=1883, keepalive=40)
+
+sw = Pin(23, Pin.IN)
+led = Pin(2, Pin.OUT)
+d = dht.DHT22(Pin(25))
+
+def sub_cb(topic, msg):
+    print((topic, msg))
+    if msg==b"apagar":
+        led.value(0)
+    if msg==b"encender":
+        led.value(1)
+    mqtt.publish(f"ap/{CLIENT_ID}/estado",str(led.value()))
+
+mqtt.set_callback(sub_cb)
+mqtt.connect()
+mqtt.subscribe(f"ap/{CLIENT_ID}/comando")
+
+def transmitir(pin):
+    mqtt.publish(f"ap/{CLIENT_ID}",datos)
+
+timer1 = Timer(1)
+timer1.init(period=20000, mode=Timer.PERIODIC, callback=transmitir)
+
+datos={}
+
+while True:
+    try:
+        d.measure()
+        temperatura=d.temperature()
+        humedad=d.humidity()
+        datos=json.dumps(OrderedDict([
+            ('temperatura',temperatura),
+            ('humedad',humedad)
+        ]))
+        print(datos)
+    except OSError as e:
+        print("sin sensor")
+    mqtt.check_msg()
+    time.sleep(5)
+
+mqtt.disconnect()
